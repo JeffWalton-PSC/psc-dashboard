b570f80e2fd66fae10afebfc4db00d2ce7045833
JeffWalton-PSC <jwalton@paulsmiths.edu>
2022-03-22 15:41:02 -0400

    add College Enrollment - Gender

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import src.pages.academic_program_enrollment_gender
 import src.pages.academic_program_enrollment_race_ethnicity
 import src.pages.admissions_current_deposits
 import src.pages.admissions_historic_data_by_stage
+import src.pages.college_enrollment_gender
 import src.pages.program_review_course_enrollment
 import src.pages.registrar_class_times
 import src.pages.registrar_course_scheduling
@@ -24,6 +25,7 @@ PAGES = {
     "Academic Program - Race/Ethnicity": src.pages.academic_program_enrollment_race_ethnicity,
     "Admissions - Current Deposits": src.pages.admissions_current_deposits,
     "Admissions - Historic Data By Stage": src.pages.admissions_historic_data_by_stage,
+    "College Enrollment - Gender": src.pages.college_enrollment_gender,
     "Program Review - Course Enrollment": src.pages.program_review_course_enrollment,
     "Registrar - Class Times": src.pages.registrar_class_times,
     "Registrar - Course Scheduling": src.pages.registrar_course_scheduling,
