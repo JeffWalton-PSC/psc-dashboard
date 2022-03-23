1f39a8783e10c473b46d28d0de6606ffce05a081
JeffWalton-PSC <jwalton@paulsmiths.edu>
2022-03-23 09:23:01 -0400

    add College Enrollment - Degree

---
+++ src/pages/college_enrollment_degree.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import altair as alt
+from pathlib import Path
+import src.pages.components
+
+
+@st.cache
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+
+def write():
+    """Used to write the page in the app.py file"""
+    with st.spinner("Loading College Enrollment - Degree ..."):
+        src.pages.components.logo()
+        st.write(
+            """
+            ## College Enrollment - Degree
+           
+"""
+        )
+
+        data_path = Path(r"\\psc-data\E\Data\Census\CensusDatabase")
+        data_file = data_path / "census_db.arr"
+
+        df = (
+            pd.read_feather(data_file)
+            .sort_values(['yearterm_sort', 'people_code_id'])
+        )
+
+        term_list = df['current_yearterm'].unique()
+        terms = st.multiselect(
+            'Select term(s):',
+            options=term_list,
+            default=[t for t in term_list if "Fall" in t],
+            )
+        # order terms
+        terms = [t for t in term_list if t in terms]
+
+        if terms:
+            selected_df = (
+                df.loc[(df['current_yearterm'].isin(terms)), 
+                ['current_yearterm', 'yearterm_sort', 'DEGREE', 'people_code_id']]
+                .groupby(['yearterm_sort', 'current_yearterm', 'DEGREE'])
+                .count()
+                .reset_index()
+                .rename(columns={'people_code_id': 'count', 'current_yearterm': 'yearterm'})
+                .sort_values(['yearterm_sort', 'DEGREE'])
+                .astype({'count': 'UInt16'})
+            )
+
+            enrollment = pd.pivot(
+                selected_df,
+                values='count',
+                index=['DEGREE'],
+                columns=['yearterm'],
+            )[terms]
+            enrollment = enrollment.fillna(0)
+
+            st.dataframe(enrollment)
+
+            st.download_button(
+                label="Download data as CSV",
+                data=convert_df(enrollment),
+                file_name=f'college_enrollment_degree.csv',
+                mime='text/csv',
+            )
+
+            col1, col2 = st.columns(2)
+
+            c1 = alt.Chart(selected_df).mark_bar().encode(
+                x='yearterm:N',
+                y=alt.Y('sum(count):Q', axis=alt.Axis(title='number of students')),
+                color='DEGREE:N',
+                tooltip=['yearterm', 'DEGREE', alt.Tooltip('sum(count):Q', title='students')],
+            )
+            with col1:
+                st.altair_chart(c1)
+            
+            c2 = alt.Chart(selected_df).transform_aggregate(
+                c='sum(count)',
+                groupby=['yearterm', 'DEGREE']
+            ).transform_joinaggregate(
+                total='sum(c)',
+                groupby=['yearterm']  
+            ).transform_calculate(
+                frac=alt.datum.c / alt.datum.total
+            ).mark_bar().encode(
+                x='yearterm:N',
+                y=alt.Y('c:Q', stack="normalize", axis=alt.Axis(format='.0%', title='percent')),
+                color='DEGREE:N',
+                tooltip=['yearterm', 'DEGREE', 
+                    alt.Tooltip('c:Q', title='total students'),
+                    alt.Tooltip('frac:Q', title='percent of students', format='.1%')],
+            )
+            with col2:
+                st.altair_chart(c2)
