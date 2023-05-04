98fe289a64a752e740df9eadd35feb9c2d8d0439
Jeff Walton <jwalton@paulsmiths.edu>
2023-05-04 17:04:39 -0400

    upgrade to Python 3.11

--- IR_dashboard.bat
+++ IR_dashboard.bat
@@ -10,12 +10,12 @@ echo "F:" 1>> F:\Applications\dashboard\logs\out.log 2>&1
 cd F:\Applications\dashboard
 echo "cd" 1>> F:\Applications\dashboard\logs\out.log 2>&1
 
-call C:\ProgramData\Anaconda3\condabin\activate.bat py310streamlit 1>> F:\Applications\dashboard\logs\out.log 2>&1
+call C:\ProgramData\Anaconda3\condabin\activate.bat py311streamlit 1>> F:\Applications\dashboard\logs\out.log 2>&1
 echo "call" 1>> F:\Applications\dashboard\logs\out.log 2>&1
 rem call conda list 1>> F:\Applications\dashboard\logs\out.log 2>&1
 
 echo "START streamlit run app.py " >> F:\Applications\dashboard\logs\out.log
-C:\ProgramData\Anaconda3\envs\py310streamlit\python.exe app.py 1>> F:\Applications\dashboard\logs\out.log 2>&1
+C:\ProgramData\Anaconda3\envs\py311streamlit\python.exe app.py 1>> F:\Applications\dashboard\logs\out.log 2>&1
 
 rem pause
 time /T >> F:\Applications\dashboard\logs\out.log
